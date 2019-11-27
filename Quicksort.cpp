afbb828be60f62fcaf57ce83865c3fac9ae7f461
Mike Mallow <mmallow1@binghamton.edu>
2019-11-27 11:02:26 -0500

    added most QS code- onto debugging now

--- Quicksort.cpp
+++ Quicksort.cpp
@@ -1,1 +1,38 @@
 #include "Quicksort.hpp"
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+int Quicksort::swap(std::vector<int> data, int low, int high){
+  std::cout << "Pre-sorted list:" << std::endl;
+  for(int i =0; i < data.size(); i++){
+    std::cout << data[i] << "->";
+  }
+  std::cout << "" << std::endl;
+  if(low < high){
+    int q;
+    q = Quicksort::partition(data, low, high);
+    Quicksort::swap(data, low, q-1);
+    Quicksort::swap(data, q+1, high);
+  }
+  std::cout << "After sorting: " << std::endl;
+  for(int i = 0; i < data.size(); i++){
+    std::cout << data[i] << "->";
+  }
+  std::cout << "" << std::endl;
+  return 0;
+}
+
+int Quicksort::partition(std::vector<int> data, int low, int high){
+  int x = data[high];
+  int i = low - 1;
+  for(j = low, j < high; j++){
+    if(data[j] < x){
+      i++;
+      std::swap(data[i], data[j]);
+    }
+  }
+  i++;
+  std::swap(data[i], data[high]);
+  return i;
+}
--- Quicksort.hpp
+++ Quicksort.hpp
@@ -1,4 +1,12 @@
 #ifndef QUICK_H
 #define QUICK_H
 
+class Quicksort{
+public:
+  int swap(std::vector<int> data, int low, int high);
+private:
+  int partition(std::vector<int> arr, int low, int high);
+
+};
+
 #endif
