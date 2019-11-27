c5a534f93be66395908fd5092709ad1060e4e68f
Mike Mallow <herewegolakers@gmail.com>
2019-11-27 11:58:29 -0500

    Fully functional QS algorithm

--- Quicksort.cpp
+++ Quicksort.cpp
@@ -1,32 +1,33 @@
-#include "Quicksort.hpp"
 #include <vector>
+#include "Quicksort.hpp"
 #include <algorithm>
 #include <iostream>
 
-int Quicksort::swap(std::vector<int> data, int low, int high){
-  std::cout << "Pre-sorted list:" << std::endl;
-  for(int i =0; i < data.size(); i++){
-    std::cout << data[i] << "->";
-  }
-  std::cout << "" << std::endl;
+int Quicksort::swap(std::vector<int> &data, int low, int high){
+ // std::cout << "Pre-sorted list:" << std::endl;
+  //for(int i =0; i < data.size(); i++){
+    //std::cout << data[i] << "->";
+  //}
+  //std::cout << "" << std::endl;
   if(low < high){
     int q;
     q = Quicksort::partition(data, low, high);
     Quicksort::swap(data, low, q-1);
     Quicksort::swap(data, q+1, high);
   }
-  std::cout << "After sorting: " << std::endl;
-  for(int i = 0; i < data.size(); i++){
-    std::cout << data[i] << "->";
-  }
-  std::cout << "" << std::endl;
+
+ // std::cout << "After sorting: " << std::endl;
+  //for(int i = 0; i < data.size(); i++){
+    //std::cout << data[i] << "->";
+  //}
+  //std::cout << "" << std::endl;
   return 0;
 }
 
-int Quicksort::partition(std::vector<int> data, int low, int high){
+int Quicksort::partition(std::vector<int> &data, int low, int high){
   int x = data[high];
   int i = low - 1;
-  for(j = low, j < high; j++){
+  for(int j = low; j < high; j++){
     if(data[j] < x){
       i++;
       std::swap(data[i], data[j]);
--- Quicksort.hpp
+++ Quicksort.hpp
@@ -1,11 +1,11 @@
 #ifndef QUICK_H
 #define QUICK_H
-
+#include <vector>
 class Quicksort{
-public:
-  int swap(std::vector<int> data, int low, int high);
-private:
-  int partition(std::vector<int> arr, int low, int high);
+	public:
+  		int swap(std::vector<int> &data, int low, int high);
+	private:
+  		int partition(std::vector<int> &arr, int low, int high);
 
 };
 
