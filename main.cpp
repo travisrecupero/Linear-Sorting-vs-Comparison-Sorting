69e0fd665cc800fdc30762dcbda195d5dc2fb94e
Mike Mallow <mmallow1@binghamton.edu>
2019-11-27 11:09:08 -0500

    started boilerplate code for main

--- main.cpp
+++ main.cpp
@@ -1,1 +1,9 @@
 #include "main.hpp"
+#include "Quicksort.hpp"
+#include <vector>
+
+int main(int argc, char * argv[]){
+  std::vector<int> list = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+  Quicksort::swap(list, 0, list.size()-1);
+  return 0;
+}
