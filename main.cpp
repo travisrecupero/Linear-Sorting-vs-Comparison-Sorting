f2d5cda481806a6ddb4deffec20e57a7df8fc2aa
Mike Mallow <herewegolakers@gmail.com>
2019-11-28 18:06:41 -0500

    Added file I/O and organized the main method better

--- main.cpp
+++ main.cpp
@@ -3,14 +3,32 @@
 #include <vector>
 #include <chrono>
 #include <iostream>
+#include <string>
+#include <fstream>
 
 int main(int argc, char * argv[]){
+	std::vector<int> inp_vec;
+	readFile(inp_vec,argv[1]);
+	std::vector<int> qs_vec(inp_vec);
+
+	//this is where the quicksort algorithm runs
 	Quicksort qs;
-	std::vector<int> list = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
-  auto start = std::chrono::high_resolution_clock::now();
-	qs.swap(list, 0, list.size()-1);
-  auto stop = std::chrono::high_resolution_clock::now();
-  auto time = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
-  std::cout << "Quicksort took a total of: " << time.count() << " microseconds to complete." << std::endl;
-  return 0;
+	auto start = std::chrono::high_resolution_clock::now();
+	qs.swap(qs_vec, 0, qs_vec.size()-1);
+	auto stop = std::chrono::high_resolution_clock::now();
+	auto time = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+	std::cout << "Quicksort took a total of: " << time.count() << " microseconds to complete." << std::endl;
+	//end of quicksort
+	return 0;
+}
+
+
+int readFile(std::vector<int> &vec, std::string inputFile){
+	std::ifstream input;
+	input.open(inputFile);
+	std::string line;
+	while(getline(input, line)){
+		vec.push_back(std::stoi(line));
+	}
+	return 0;
 }
--- main.hpp
+++ main.hpp
@@ -1,4 +1,8 @@
 #ifndef MAIN
 #define MAIN
+#include <vector>
+#include <string>
+
+int readFile(std::vector<int> &vec, std::string inputFile);
 
 #endif
