c7285fb5b412fd25ba8b7b69f699bb9a26c8c4ef
Travis Recupero <trecupe1@binghamton.edu>
2019-11-29 14:58:07 -0500

    fixed sort, should be fully working and compatable with main

--- Selection.cpp
+++ Selection.cpp
@@ -2,19 +2,25 @@
 #include <iostream>
 #include <cstdlib>
 #include <vector>
+#include <algorithm>
+
+
 
 void Selection::sort(vector<int>& vect){
-	
+
 	int minimumIndex = 0;
-	
+
 	for(int i = 0; i < vect.size() - 1; i++){
 		minimumIndex = i;
 		for(int j = i + 1; j < vect.size(); j++){
-			if(vect.at(j) < vect.at(minimumIndex)){
+			if(vect[j] < vect[minimumIndex]){
 				minimumIndex = j;
 			}
+		}
+		if(minimumIndex != i){
 			swap(vect[minimumIndex], vect[i]);
 		}
 	}
+
 }
 
